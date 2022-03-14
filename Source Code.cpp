74867119e2f65315a4b9bd529ee65182d1925eeb
wanyeung <69677029+wanyeung@users.noreply.github.com>
2022-03-14 12:40:39 +0800

    Create Source Code.cpp

---
+++ Source Code.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void welcome();
+void game();
+void config();
+void feature();
+void credit();
+void exit();
+
+//Main Menu
+int main()
+{
+	
+	return 0;
+}
+
+//Welcome Message
+void welcome(){
+  
+}
+
+//Start the game
+void game(){
+  
+}
+
+//setting
+void config(){
+  
+}
+
+//Additional Feature
+void feature(){
+  
+}
+
+//Credit
+void credit(){
+  
+}
+
+//Exit
+void exit(){
+  
+}
